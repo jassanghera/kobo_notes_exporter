cd0fe607589e1e79dacd990ce486440ddfce978a
jassanghera <jaspreetsanghera95@gmail.com>
2026-01-29 19:08:35 -0500

    Update getBookData.py
    prints chapter title and corresponding highlights in correct order

--- getBookData.py
+++ getBookData.py
@@ -67,9 +67,9 @@ def get_kepub_chapters():
     return df
 
 df_kepub_chapters = get_kepub_chapters()
-print("KEPUB Chapters:")
-print(get_kepub_chapters().head())
-print("\n")
+# print("KEPUB Chapters:")
+# print(get_kepub_chapters().head())
+# print("\n")
 
 
 # make df of highlights from the Bookmark table with their ContentID, VolumeID (=BookID), and Text
@@ -86,18 +86,18 @@ df_highlights = get_all_highlights()
 # print("\n")
 
 # ----------------------------------------------------------------------------------
-# PRINTING
+# PRINTING for testing
 # ----------------------------------------------------------------------------------
 
-# print kepub chapters VolumeIndex column sample
-print("KEPUB Chapters VolumeIndex Sample:")
-print(df_kepub_chapters[['Title', 'VolumeIndex']].tail(10))
-print("\n")
+# # print kepub chapters VolumeIndex column sample
+# print("KEPUB Chapters VolumeIndex Sample:")
+# print(df_kepub_chapters[['Title', 'VolumeIndex']].head(10))
+# print("\n")
 
-# print epub chapters VolumeIndex column sample
-print("EPUB Chapters VolumeIndex Sample:")
-print(df_epub_chapters[['Title', 'VolumeIndex']].tail(10))
-print("\n")
+# # print epub chapters VolumeIndex column sample
+# print("EPUB Chapters VolumeIndex Sample:")
+# print(df_epub_chapters[['Title', 'VolumeIndex']].head(10))
+# print("\n")
 
 # --------------------------------------------------------------------------------
 # MATCHING KEPUB CONTENTIDs
@@ -108,114 +108,120 @@ kepub_id_lookup = dict(
     zip(df_kepub_chapters['ContentID'], df_kepub_chapters['VolumeIndex'])
 )
 
+# pass in a highlight ContentID, return the VolumeIndex if prefix match kepub ContentID
 def lookup_kepub_index(content_id):
 
     for ch_id, vol_idx in kepub_id_lookup.items():
-        if content_id.startswith(ch_id):
+        if ch_id.startswith(content_id):
             return vol_idx
     return None
 
 # print sample of dictionary
-print("Kepub ContentID to VolumeIndex Lookup Sample:")
-for i, (ch_id, vol_idx) in enumerate(kepub_id_lookup.items()):
-    if i >= 5:
-        break
-    print(f"{ch_id} -> {vol_idx}")
+# print("Kepub ContentID to VolumeIndex Lookup Sample:")
+# for i, (ch_id, vol_idx) in enumerate(kepub_id_lookup.items()):
+#     if i >= 5:
+#         break
+#     print(f"{ch_id} -> {vol_idx}")
 
+# print('\n')
+
+# -----------------------------------------------------------------------------------
+# ATTACH KEPUB VOLUMEINDEX TO HIGHLIGHTS (epub as backup)
 # ----------------------------------------------------------------------------------
-# JOIN CHAPTER INDICES TO HIGHLIGHTS
-# ----------------------------------------------------------------------------------
 
-def join_hl_to_VolumeIndex():
+# add kepub VolumeIndex column to highlights df manually using the lookup function
+df_highlights['VolumeIndex'] = df_highlights['ContentID'].apply(lookup_kepub_index)
+# print("Highlights with KEPUB VolumeIndex:")
+# print(df_highlights[['ContentID', 'VolumeIndex']].tail(20))
+# print("\n")
 
-    # create minimal kepub chapter index - only keep ContentID and VolumeIndex
-    df_kepub_index = (
-        df_kepub_chapters[['ContentID', 'VolumeIndex']]
-        .rename(columns={'VolumeIndex': 'VolumeIndex_kepub'})
-    )
+# insert epub VolumeIndex as backup where kepub VolumeIndex is missing, consider exact match of ContentID
+    
+rowidx_vidx = {} # (row_index : volume_index), rows from highlight table, vidx from epub chapter VolumeIndex
 
-    # create minimal epub chapter index
-    df_epub_index = (
-        df_epub_chapters[['ContentID', 'VolumeIndex']]
-        .rename(columns={'VolumeIndex': 'VolumeIndex_epub'})
-    )
+df1 = df_highlights.iterrows()  
+for _i, row in df1:
+    ContentID = row['ContentID']
+    VolumeIndex = row['VolumeIndex']
+    if pd.isna(VolumeIndex):                                                            # if highlight VolumeIndex is NaN
+        epub_row = df_epub_chapters.loc[df_epub_chapters['ContentID'] == ContentID]     # get epub_chapter row with matching ContentID
+        epub_vidx = epub_row.iloc[0]['VolumeIndex']                                     # get value of epub VolumeIndex
+        # print(epub_vidx)
 
-    # join highlights with kepub VolumeIndex on ContentID exact match
+        rowidx_vidx[_i] = epub_vidx    # store epub VolumeIndex in dict with corresponding highlight index
+        # print(rowidx_vidx)
 
-    df_hl_with_kepub = df_highlights.merge(
-        df_kepub_index,
-        on='ContentID',
-        how='left'
-    )
+        # print(_i)
+        # df_highlights.at[_i, 'VolumeIndex'] = epub_vidx
+        # print(df_highlights.loc[_i,'VolumeIndex'])
+        
+# print("Highlights with KEPUB VolumeIndex AFTER EPUB BACKUP:")
+# print(df_highlights[['ContentID', 'VolumeIndex']].tail(20))
+# print("\n")
 
-    df_hl_with_all_index = df_hl_with_kepub.merge(
-        df_epub_index,
-        on='ContentID',
-        how='left'
-    )
+# update highlight VolumeIndex column with vidx values from dict at row with index rowidx
 
-    return df_hl_with_all_index
+for idx, val in rowidx_vidx.items():
+    print(idx, val)
+    df_highlights.at[idx, 'VolumeIndex'] = val
 
-df_highlights_with_indices = join_hl_to_VolumeIndex()
-print("Highlights with Chapter Indices:")
-print(df_highlights_with_indices.tail())
+print("Highlights with KEPUB VolumeIndex AFTER EPUB BACKUP ATTEMPT 2:")
+print(df_highlights[['ContentID', 'VolumeIndex']].tail(10))
 print("\n")
 
-# print first row in readable format
-check_row = df_highlights_with_indices.iloc[100]
-print("First Highlight with Indices:")
-print(check_row)
-print("\n")
 
-# sanity check
-print("Sample of Highlights with Volume Indices:")
-print(df_highlights_with_indices[['VolumeIndex_kepub', 'VolumeIndex_epub']].tail(10))
+# -----------------------------------------------------------------------------------
+# PRINTING FOR TESTING
+# ----------------------------------------------------------------------------------
 
+# print("HIGHLIGHT:")
+# print(df_highlights['ContentID'].iloc[0])
+# print("\n")
 
+# print("\nCHAPTER:")
+# print(df_kepub_chapters['ContentID'].iloc[0])
+# print("\n")
 
+# sample_highlight = df_highlights['ContentID'].iloc[0]
+
+# matches = [
+#     ch_id for ch_id in df_kepub_chapters['ContentID']
+#     if ch_id.startswith(sample_highlight)
+# ]
+
+# print("MATCHES:")
+# print(len(matches))
+# print(matches[:5])
+# print("\n")
 
 #----------------------------------------------------------------------------------
 # SORT HIGHLIGHTS BY CHAPTER INDICES
 #----------------------------------------------------------------------------------
 
-def sort_highlights_by_VolumeIndex():
-    pass
+def sort_highlights(df):
+    return df.sort_values(by=['VolumeID', 'VolumeIndex'])
 
+df_highlights_sorted = sort_highlights(df_highlights)
 
-# --------------------------------------------------------------------------------------------------
-# HIGHLIGHT BOOKS with COUNTS
-# --------------------------------------------------------------------------------------------------
-
-# def get_highlight_counts():
-#     highlight_counts = df_highlights['VolumeID'].value_counts().reset_index() # value_counts gives a Series, reset_index to convert to DataFrame
-
-#     # rename columns
-#     highlight_counts.columns = ['VolumeID', 'HighlightCount']
-
-#     # merge with book data to get titles and authors
-#     books_with_highlights = highlight_counts.merge(df_books, left_on='VolumeID', right_on='ContentID', how='left')
-
-#     # select relevant columns
-#     return books_with_highlights[['Title', 'Attribution', 'HighlightCount']]
-
-
-# print("Books with Highlights:")
-# print(get_highlight_counts())
+# print("Sorted Highlights Sample:")
+# print(df_highlights_sorted[['ContentID', 'VolumeIndex']].tail(10))
 # print("\n")
 
+# print(df_highlights_sorted.iloc[122])
 
 # ----------------------------------------------------------------------------------
 # CHAPTERS & HIGHLIGHTS FOR A GIVEN BOOK
 # ----------------------------------------------------------------------------------
 
 VolumeID_list = df_highlights['VolumeID'].unique().tolist()
-sample_VolumeID = VolumeID_list[2]
+sample_VolumeID = VolumeID_list[5]
 
 def map_chapters_to_highlights(volume_id):
 
+
     # get all highlights for the given VolumeID
-    book_highlights = df_highlights[df_highlights['VolumeID'] == volume_id]
-    # print(book_highlights)
+    book_highlights = df_highlights_sorted[df_highlights_sorted['VolumeID'] == volume_id]
+    
 
     # create dict to map chapter titles to list of highlights
     chapters_to_highlights = {}
@@ -242,39 +248,64 @@ def map_chapters_to_highlights(volume_id):
 
     return chapters_to_highlights
 
-# example usage
-mapped_highlights = map_chapters_to_highlights(sample_VolumeID)
-print("Mapped Highlights to Chapters:")
-print('\n')
-for chapter, highlights in mapped_highlights.items():
-    print(f"Chapter: {chapter}")
-    for highlight in highlights:
-        print(f"- {highlight}")
+
+# pretty print chapter titles & highlights
+def print_mapped_highlights(mapped_highlights):
+
+    print("Mapped Highlights to Chapters:")
+    print('\n')
+
+    for chapter, highlights in mapped_highlights.items():
+        print(f'Chapter: {chapter}')
+        for highlight in highlights:
+            print(f'- {highlight}')
         print("\n")
     print("\n")
-print("\n")
+
+# example usage
+mapped_highlights = map_chapters_to_highlights(sample_VolumeID)
+print_mapped_highlights(mapped_highlights)
 
 
+# --------------------------------------------------------------------------------------------------
+# GET LIST OF HIGHLIGHTED BOOKS WITH COUNT OF HIGHLIGHTS FOR EACH
+# --------------------------------------------------------------------------------------------------
 
+def get_highlight_counts():
+    highlight_counts = df_highlights['VolumeID'].value_counts().reset_index() # value_counts gives a Series, reset_index to convert to DataFrame
+
+    # rename columns
+    highlight_counts.columns = ['VolumeID', 'HighlightCount']
+
+    # merge with book data to get titles and authors
+    books_with_highlights = highlight_counts.merge(df_books, left_on='VolumeID', right_on='ContentID', how='left')
+
+    # select relevant columns
+    return books_with_highlights[['Title', 'Attribution', 'HighlightCount']]
+
+
+# print("Books with Highlights:")
+# print(get_highlight_counts())
+# print("\n")
 
 
 # # --------------------------------------------------------------------------------------------------
 # # GET LIST OF CHAPTER TITLES FOR A GIVEN BOOK
-# # ------------------------------------------------------------------------------------------------__
+# # --------------------------------------------------------------------------------------------------
 
-# # write a function that gets the chapter titles for a given VolumeID from either the kepub or epub chapters dataframes
-# def get_chapter_titles(volume_id):
-#     # try kepub chapters first
-#     kepub_chapters = df_kepub_chapters[df_kepub_chapters['BookID'] == volume_id].sort_values('VolumeIndex')
-#     if not kepub_chapters.empty:
-#         return kepub_chapters['Title'].tolist()
+# write a function that gets the chapter titles for a given VolumeID from either the kepub or epub chapters dataframes
+def get_chapter_titles(volume_id):
+    # try kepub chapters first
+    kepub_chapters = df_kepub_chapters[df_kepub_chapters['BookID'] == volume_id].sort_values('VolumeIndex')
+    if not kepub_chapters.empty:
+        return kepub_chapters['Title'].tolist()
 
-#     # if no kepub chapters, try epub chapters
-#     epub_chapters = df_epub_chapters[df_epub_chapters['BookID'] == volume_id].sort_values('VolumeIndex')
-#     if not epub_chapters.empty:
-#         return epub_chapters['Title'].tolist()
+    # if no kepub chapters, try epub chapters
+    epub_chapters = df_epub_chapters[df_epub_chapters['BookID'] == volume_id].sort_values('VolumeIndex')
+    if not epub_chapters.empty:
+        return epub_chapters['Title'].tolist()
 
-#     return []
+    return []
 
 
 # # example usage
@@ -285,6 +316,27 @@ print("\n")
 # for title in chapter_titles:
 #     print(f"- {title}")
 # print("\n")
+
+
+# -------------------------------------------------------------------------------------------------
+# getters - book title, author
+# -------------------------------------------------------------------------------------------------
+
+def get_book_title(volume_id):
+    book = df_books[df_books['ContentID'] == volume_id]
+    title = book.iloc[0]['Title']
+    return title
+
+# book_title = get_book_title(sample_VolumeID)
+# print(f'The sample book is: {book_title}')
+
+def get_book_author(volume_id):
+    book = df_books[df_books['ContentID'] == volume_id]
+    author = book.iloc[0]['Attribution']
+    return author
+
+# book_author = get_book_author(sample_VolumeID)
+# print(f'The same book author is: {book_author}')
 
 
 
